29cb2ba1ee2feaeac8bf8d35333acb1e0f0ce59c
CatWithPaws <valentunkulakov6@gmail.com>
2020-08-09 09:53:14 +0300

    too many changes to conclude

---
+++ Assets/Editor/CameraInspector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(CameraFollow))]
+public class CameraInspector : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		CameraFollow cameraFollow = (CameraFollow)target;
+		DrawDefaultInspector();
+		if(GUILayout.Button("Set Bottom Left Side"))
+		{
+			cameraFollow.SetRestrictBotLeftSide();
+		}
+		if(GUILayout.Button("Set Top Right Side"))
+		{
+			cameraFollow.SetRestrictTopRightSide();
+		}
+	}
+}
---
+++ Assets/Scripts/CameraMove/CameraFollow.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Camera))]
+public class CameraFollow : MonoBehaviour
+{
+    [Header("CameraFollow")]
+    [SerializeField] float _dampTime = 0.15f;
+    private Vector3 _velocity = Vector3.zero;
+    [SerializeField] private Transform _target;
+    [Header("Camera Move Area")]
+    [SerializeField] Vector2 restrictBottomLeftSide;
+    [SerializeField] Vector2 restrictTopRightSide;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+    void LateUpdate()
+    {
+        if (_target)
+        {
+            //Calculate where camera have to move
+            Vector3 cameraPoint = _camera.transform.position;
+            Vector3 targetPoint = _target.position;
+
+            //Make smooth transition from current camera position to target position
+            Vector3 willCameraPosition = Vector3.LerpUnclamped(cameraPoint, new Vector3(targetPoint.x,targetPoint.y,cameraPoint.z), Time.deltaTime * 5);
+
+            //Clamping area to restrict camera move on no playable places
+            willCameraPosition.x = Mathf.Clamp(willCameraPosition.x, restrictBottomLeftSide.x, restrictTopRightSide.x);
+            willCameraPosition.y = Mathf.Clamp(willCameraPosition.y, restrictBottomLeftSide.y, restrictTopRightSide.y);
+
+            //Set camera position
+            _camera.transform.position = willCameraPosition;
+
+            
+
+        }
+
+    }
+    
+    public void SetRestrictBotLeftSide()
+	{
+        restrictBottomLeftSide = transform.position;
+	}
+    public void SetRestrictTopRightSide()
+    {
+        restrictTopRightSide = transform.position;
+    }
+}
